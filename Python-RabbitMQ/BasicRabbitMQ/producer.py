4b2f1ea6013dd70d2331bb69cb0a9bd3d83b9dd8
Amit Chawla <amit.chawla@blackbaud.me>
2022-02-19 01:41:06 +0530

    add comments

--- Python-RabbitMQ/BasicRabbitMQ/producer.py
+++ Python-RabbitMQ/BasicRabbitMQ/producer.py
@@ -1,14 +1,18 @@
 import pika
-
+# create connection
 connection = pika.BlockingConnection(pika.ConnectionParameters('localhost')) # connected to a broker on localhost, add IP address for remote server.
+
+# create a channel
 channel = connection.channel()
 
-channel.queue_declare(queue='hello') # send message to the queue named "hello"
+# send message to the queue named "hello"
+channel.queue_declare(queue='hello') 
 
 # Use default exchange and publish to hello queue
 message = 'Hello Again'
-channel.basic_publish(exchange='', routing_key='hello',  body=message)
 
+# 4 publish using channel
+channel.basic_publish(exchange='', routing_key='hello',  body=message)
 print("[x] Sent '%r'" % message)
 
 connection.close()
